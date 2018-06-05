1eadf2b9767a89650949009e66573afd191c813d
ciepielapawel <39746915+ciepielapawel@users.noreply.github.com>
2018-06-05 18:43:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+        document.querySelector("header").classList.toggle("nav-opened")
+    },
+
+    false);
